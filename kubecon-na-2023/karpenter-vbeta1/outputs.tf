af7b38dfee05045ed562ab6375008b0d5af94da8
Rodrigo Bersa <rodrigo.bersa@gmail.com>
2024-01-30 21:24:32 -0500

    Addin NoVA MeetUp Jan2024

--- kubecon-na-2023/karpenter-vbeta1/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "configure_kubectl" {
-  description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
-  value       = "aws eks --region ${local.region} update-kubeconfig --name ${module.eks.cluster_name}"
-}
-
-output "karpenter" {
-  description = "Karpenter settings to be consumed by external modules"
-  value       = module.eks_blueprints_addons.karpenter
-}
